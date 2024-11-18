e35e2e7c55b0095d8dad53a8ddf23473de58a004
Xuan-Toan-13 <toan301310@gmail.com>
2024-11-18 23:41:08 +0700

    trang chu

---
+++ app/src/main/java/com/example/admin_stadium/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.admin_stadium;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Set the XML layout
+    }
+
+
+}
