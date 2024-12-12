cfa8f01fff7e57f0e6157fdf58d6ecd86518e98e
Xuan-Toan-13 <toan301310@gmail.com>
2024-12-12 15:13:33 +0700

    frontend

---
+++ app/src/main/java/com/example/admin_stadium/StadiumDetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.admin_stadium;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StadiumDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stadium_detail);
+
+        // Lấy các TextView để hiển thị thông tin chi tiết
+
+    }
+}
